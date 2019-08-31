6ededcb188dcf4d0b205507cc659abc2a73c98d2
talitast <talitatravassos.tt@gmail.com>
2019-08-31 17:29:51 -0300

    fix a boring error

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonListComponent implements OnInit {
 
-  pokemonList: any[]
+  pokemonList: any[] = []
 
   constructor(private pokemonServive: PokemonService) {
 
