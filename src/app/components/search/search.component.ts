eece3d92c6727dbca4d15831c1d0cd66207c5b6a
talitast <talitatravassos.tt@gmail.com>
2019-09-01 12:10:01 -0300

    added search error notification

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from 'src/app/services/pokemon-service.service';
 import { FormGroup } from '@angular/forms';
+import { NotificationsService } from 'src/app/services/notifications.service';
 
 @Component({
   selector: 'search',
@@ -12,14 +13,24 @@ export class SearchComponent implements OnInit {
   isSearch: boolean
   pokemon: any 
 
-  constructor(private pokemonServive: PokemonService) { }
+  constructor(
+    private pokemonServive: PokemonService,  
+    private notificationService: NotificationsService) { }
 
   submit(search: FormGroup){
     this.pokemonServive.searchPokemon(search.value.searchPokemon)
-      .subscribe( (res: any) => {
-        console.log(res)
-        this.pokemon = res
-        this.isSearch = true
+      .subscribe( 
+        (res: any) => {
+          console.log(res)
+          this.pokemon = res
+          this.isSearch = true
+      },
+      (error: Response) => {
+        if(error.status === 404){
+          this.notificationService.errorNotification("Pokemon not found :/", "Sorry")
+        } else {
+          this.notificationService.errorNotification("Something's wrong :/", "Sorry")
+        }
       })
   }
 
